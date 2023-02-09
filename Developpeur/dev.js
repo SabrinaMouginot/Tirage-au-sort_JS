9708d83f61a62dcafea45ab72cbc0ec3a3db7e86
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-09 02:41:52 +0100

    1st commit

---
+++ Developpeur/dev.js
@@ -0,0 +1,3 @@
+document.write("<h1> source: Onisep </h1> ");
+document.write("<br>&nbsp;</br>");
+document.write(" \" Expert des langages informatiques, le développeur informatique traduit la demande d'un client en lignes de code informatique. La révolution numérique le place parmi les professionnels les plus recherchés, surtout s'il sait s'adapter et élargir ses compétences. \" ");
